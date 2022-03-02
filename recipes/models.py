b32c65441b17a54cf9a1e0927f1feef717742ee4
Luis-Felipe-N <luisj2felipe09@gmail.com>
2022-03-02 10:45:00 -0300

    creating recipes

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,69 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(
+        max_length=196
+    )
+
+    def __str__(self):
+        return self.name
+    
+
+
+
+class Recipe(models.Model):
+    title = models.CharField(
+        max_length=96
+    )
+
+    description = models.CharField(
+        max_length=196
+    )
+
+    slug = models.SlugField()
+
+    preparation_time = models.IntegerField()
+
+    preparation_time_unit = models.CharField(
+        max_length=96
+    )
+
+    servings = models.IntegerField()
+
+    servings_unit = models.CharField(
+        max_length=96
+    )
+
+    preparation_steps = models.TextField()
+
+    preparation_steps_is_html = models.BooleanField(default=False)
+ 
+    create_at = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    update_at = models.DateTimeField(
+        auto_now=True
+    )
+
+    is_published = models.BooleanField(
+        default=False
+        )
+
+    cover = models.ImageField(
+        upload_to='recipes/cover/%Y/%m/%d/'
+    )
+
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        null=True
+    )
+
+    author = models.ForeignKey(
+        User,
+          on_delete=models.SET_NULL,
+        null=True        
+    )
+
--- recipes/views.py
+++ recipes/views.py
@@ -1,15 +1,76 @@
 from django.shortcuts import render
+from django.utils import timezone
 
 def home(request):
-    context = {
 
+    recipes = [
+        {
+            "title": "torta de Abobora",
+            "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis non elementum elit. In quis justo nec nibh tincidunt lacinia ac vitae lacus. Suspendisse nec est tellus. Nunc id magna sed ligula hendrerit tristique. Mauris dignissim non dui sed condimentum. Sed eget vestibulum massa. Nullam scelerisque, felis id iaculis porttitor, nisl velit scelerisque purus, a auctor eros est sed nisi. Donec sit amet magna ac tortor laoreet fermentum in in purus.",
+            "preparation_time": 10,
+            "preparation_time_unit": 'Minutos',
+            "servings": 1,
+            "servings_unit": "Prato",
+            "cover": {
+                "url": 'https://images.pexels.com/photos/7125710/pexels-photo-7125710.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
+            },
+            "author": {
+                "first_name": "Luis Felipe",
+                "last_nam": "Nunes de Carvalho"
+            },
+            "category": "Café da manhã",
+            "created_at": timezone.now(),
+            "preparation_steps": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis non elementum elit. In quis justo nec nibh tincidunt lacinia ac vitae lacus. Suspendisse nec est tellus. Nunc id magna sed ligula hendrerit tristique. Mauris dignissim non dui sed condimentum. Sed eget vestibulum massa. Nullam scelerisque, felis id iaculis porttitor, nisl velit scelerisque purus, a auctor eros est sed nisi. Donec sit amet magna ac tortor laoreet fermentum in in purus."
+        },
+        {
+            "title": "Hamburge",
+            "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis non elementum elit. In quis justo nec nibh tincidunt lacinia ac vitae lacus. Suspendisse nec est tellus. Nunc id magna sed ligula hendrerit tristique. Mauris dignissim non dui sed condimentum. Sed eget vestibulum massa. Nullam scelerisque, felis id iaculis porttitor, nisl velit scelerisque purus, a auctor eros est sed nisi. Donec sit amet magna ac tortor laoreet fermentum in in purus.",
+            "preparation_time": 8,
+            "preparation_time_unit": 'Minutos',
+            "servings": 1,
+            "servings_unit": "Prato",
+            "cover": {
+                "url": 'https://images.pexels.com/photos/1639557/pexels-photo-1639557.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
+            },
+            "author": {
+                "fisrt_name": "João Felipe",
+                "last_nam": "Nunes de Carvalho"
+            },
+            "category": "Lanche",
+            "created_at": timezone.now(),
+            "preparation_steps": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis non elementum elit. In quis justo nec nibh tincidunt lacinia ac vitae lacus. Suspendisse nec est tellus. Nunc id magna sed ligula hendrerit tristique. Mauris dignissim non dui sed condimentum. Sed eget vestibulum massa. Nullam scelerisque, felis id iaculis porttitor, nisl velit scelerisque purus, a auctor eros est sed nisi. Donec sit amet magna ac tortor laoreet fermentum in in purus."
+        }
+    ]
+
+
+    context = {
+        "recipes": recipes
     }
 
     return render(request, 'recipes/pages/index.html', context)
 
 def recipes(request, id):
     context = {
-        "id": id
+        "recipe": {
+            "id": id,
+            "title": "Hamburge",
+            "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis non elementum elit. In quis justo nec nibh tincidunt lacinia ac vitae lacus. Suspendisse nec est tellus. Nunc id magna sed ligula hendrerit tristique. Mauris dignissim non dui sed condimentum. Sed eget vestibulum massa. Nullam scelerisque, felis id iaculis porttitor, nisl velit scelerisque purus, a auctor eros est sed nisi. Donec sit amet magna ac tortor laoreet fermentum in in purus.",
+            "preparation_time": 8,
+            "preparation_time_unit": 'Minutos',
+            "servings": 1,
+            "servings_unit": "Prato",
+            "cover": {
+                "url": 'https://images.pexels.com/photos/1639557/pexels-photo-1639557.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
+            },
+            "author": {
+                "fisrt_name": "João Felipe",
+                "last_nam": "Nunes de Carvalho"
+            },
+            "category": "Lanche",
+            "created_at": timezone.now(),
+            "preparation_steps": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis non elementum elit. In quis justo nec nibh tincidunt lacinia ac vitae lacus. Suspendisse nec est tellus. Nunc id magna sed ligula hendrerit tristique. Mauris dignissim non dui sed condimentum. Sed eget vestibulum massa. Nullam scelerisque, felis id iaculis porttitor, nisl velit scelerisque purus, a auctor eros est sed nisi. Donec sit amet magna ac tortor laoreet fermentum in in purus.linebreaksbr¶"
+        },
+        "is_details": True
     }
 
     return render(request, 'recipes/pages/recipe-view.html', context)
